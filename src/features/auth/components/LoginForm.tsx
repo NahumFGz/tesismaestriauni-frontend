ea2e3ca2a5e88f85d19e74a76bc07b0c236be370
NahumFGz <ngutierrez2507@gmail.com>
2025-05-10 06:18:12 -0500

    feat: :recycle: Refactorización de LoginPage

---
+++ src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,143 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Button, Input, Checkbox, Link, Divider } from '@heroui/react'
+import { Icon } from '@iconify/react'
+import { useMutation } from '@tanstack/react-query'
+import { getProfile, login } from '../../../services/auth'
+import { useAuthStore, type UserProfile } from '../../../store/authStore'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+type LoginFormData = {
+  email: string
+  password: string
+  remember: boolean
+}
+
+type LoginResponseType = {
+  token: string
+  refreshToken: string
+  profile: UserProfile
+  rememberMe: boolean
+}
+
+export function LoginForm() {
+  const [isVisible, setIsVisible] = React.useState(false)
+  const toggleVisibility = () => setIsVisible(!isVisible)
+  const setAuth = useAuthStore((state) => state.setAuth)
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginFormData>({
+    defaultValues: {
+      email: '',
+      password: '',
+      remember: false
+    }
+  })
+
+  const mutation = useMutation<LoginResponseType, Error, LoginFormData>({
+    mutationFn: async (data: LoginFormData) => {
+      const tokensResponse = await login(data)
+      const profileData = await getProfile()
+
+      // Mapear los nombres de los tokens correctamente
+      return {
+        token: tokensResponse.accessToken,
+        refreshToken: tokensResponse.refreshToken,
+        profile: profileData,
+        rememberMe: data.remember
+      }
+    },
+    onSuccess: ({ token, refreshToken, profile, rememberMe }) => {
+      setAuth({ token, refreshToken, profile, rememberMe })
+      navigate('/chat/conversation', { replace: true })
+    },
+    onError: (error) => {
+      console.error('❌ Error de login:', error)
+      toast.error('Credenciales inválidas')
+    }
+  })
+
+  const onSubmit = (data: LoginFormData) => {
+    mutation.mutate(data)
+    console.log(errors)
+  }
+
+  return (
+    <>
+      <form className='flex flex-col gap-3' onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          isRequired
+          label='Email Address'
+          placeholder='Enter your email'
+          type='email'
+          variant='bordered'
+          {...register('email', { required: 'Email is required' })}
+        />
+
+        <Input
+          isRequired
+          endContent={
+            <button type='button' onClick={toggleVisibility}>
+              <Icon
+                className='pointer-events-none text-2xl text-default-400'
+                icon={isVisible ? 'solar:eye-closed-linear' : 'solar:eye-bold'}
+              />
+            </button>
+          }
+          label='Password'
+          placeholder='Enter your password'
+          type={isVisible ? 'text' : 'password'}
+          variant='bordered'
+          {...register('password', { required: 'Password is required' })}
+        />
+
+        <div className='flex w-full items-center justify-between px-1 py-2'>
+          <Checkbox size='sm' {...register('remember')}>
+            Remember me
+          </Checkbox>
+          <Link className='text-default-500' href='#' size='sm' isDisabled>
+            Forgot password?
+          </Link>
+        </div>
+
+        <Button className='w-full' color='primary' type='submit'>
+          Log In
+        </Button>
+      </form>
+
+      <div className='flex items-center gap-4'>
+        <Divider className='flex-1' />
+        <p className='shrink-0 text-tiny text-default-500'>OR</p>
+        <Divider className='flex-1' />
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <Button
+          startContent={<Icon icon='flat-color-icons:google' width={24} />}
+          variant='bordered'
+          isDisabled
+        >
+          Continue with Google
+        </Button>
+        <Button
+          startContent={<Icon className='text-default-500' icon='fe:github' width={24} />}
+          variant='bordered'
+          isDisabled
+        >
+          Continue with Github
+        </Button>
+      </div>
+
+      <p className='text-center text-small'>
+        Need to create an account?&nbsp;
+        <Link href='#' size='sm' isDisabled>
+          Sign Up
+        </Link>
+      </p>
+    </>
+  )
+}
